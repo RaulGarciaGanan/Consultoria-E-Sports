bb4f9d4226f6082d25ed5ad5c7a6b7254d77035f
Unknown <raul.garcia.ganan@ikasle.egibide.org>
2018-04-26 14:03:04 +0200

    Vistas casi terminadas

--- PROGRAM/Proyecto/src/MisVentanas/Principal.java
+++ PROGRAM/Proyecto/src/MisVentanas/Principal.java
@@ -16,6 +16,8 @@ public class Principal extends javax.swing.JFrame {
      */
     public Principal() {
         initComponents();
+        
+        setLocationRelativeTo(null);
     }
 
     /**
@@ -27,20 +29,193 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenu5 = new javax.swing.JMenu();
+        jCheckBoxMenuItem2 = new javax.swing.JCheckBoxMenuItem();
+        jMenu10 = new javax.swing.JMenu();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenu3 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenu4 = new javax.swing.JMenu();
+        jMenuItem6 = new javax.swing.JMenuItem();
+        jMenuItem7 = new javax.swing.JMenuItem();
+        jCheckBoxMenuItem1 = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItem3 = new javax.swing.JCheckBoxMenuItem();
+        jMenu6 = new javax.swing.JMenu();
+        jMenu7 = new javax.swing.JMenu();
+        jCheckBoxMenuItem4 = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItem5 = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItem6 = new javax.swing.JCheckBoxMenuItem();
+        jMenu8 = new javax.swing.JMenu();
+        jCheckBoxMenuItem7 = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItem8 = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItem9 = new javax.swing.JCheckBoxMenuItem();
+        jMenu9 = new javax.swing.JMenu();
+        jCheckBoxMenuItem10 = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItem11 = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItem12 = new javax.swing.JCheckBoxMenuItem();
+        jMenu11 = new javax.swing.JMenu();
+        jCheckBoxMenuItem13 = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItem14 = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItem15 = new javax.swing.JCheckBoxMenuItem();
+        jMenu12 = new javax.swing.JMenu();
+        jCheckBoxMenuItem16 = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItem17 = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItem18 = new javax.swing.JCheckBoxMenuItem();
+
+        jMenuItem4.setText("jMenuItem4");
+
+        jMenuItem5.setText("jMenuItem5");
+
+        jMenu5.setText("jMenu5");
+
+        jCheckBoxMenuItem2.setSelected(true);
+        jCheckBoxMenuItem2.setText("jCheckBoxMenuItem2");
+
+        jMenu10.setText("jMenu10");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/MisVentanas/FondoPrincipal.jpg"))); // NOI18N
 
         jMenu1.setText("Calendario");
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Equipos");
+
+        jMenuItem1.setText("jMenuItem1");
+        jMenu2.add(jMenuItem1);
+
         jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Dueño");
+
+        jMenuItem2.setText("Nuevo Equipo");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jMenuItem2);
+
+        jMenu4.setText("Gestionar equipos");
+
+        jMenuItem6.setText("Eliminar Equipo");
+        jMenu4.add(jMenuItem6);
+
+        jMenuItem7.setText("Modificar Equipos");
+        jMenu4.add(jMenuItem7);
+
+        jMenu3.add(jMenu4);
+
+        jCheckBoxMenuItem1.setText("Jornada");
+        jMenu3.add(jCheckBoxMenuItem1);
+
+        jCheckBoxMenuItem3.setText("Clasificacion");
+        jMenu3.add(jCheckBoxMenuItem3);
+
+        jMenuBar1.add(jMenu3);
+
+        jMenu6.setText("Administrador");
+
+        jMenu7.setText("Jugadores");
+
+        jCheckBoxMenuItem4.setText("Nuevo");
+        jCheckBoxMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBoxMenuItem4ActionPerformed(evt);
+            }
+        });
+        jMenu7.add(jCheckBoxMenuItem4);
+
+        jCheckBoxMenuItem5.setText("Eliminar");
+        jCheckBoxMenuItem5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBoxMenuItem5ActionPerformed(evt);
+            }
+        });
+        jMenu7.add(jCheckBoxMenuItem5);
+
+        jCheckBoxMenuItem6.setText("Modificar");
+        jMenu7.add(jCheckBoxMenuItem6);
+
+        jMenu6.add(jMenu7);
+
+        jMenu8.setText("Equipos");
+
+        jCheckBoxMenuItem7.setText("Nuevo");
+        jMenu8.add(jCheckBoxMenuItem7);
+
+        jCheckBoxMenuItem8.setText("Eliminar");
+        jMenu8.add(jCheckBoxMenuItem8);
+
+        jCheckBoxMenuItem9.setText("Modificar");
+        jMenu8.add(jCheckBoxMenuItem9);
+
+        jMenu6.add(jMenu8);
+
+        jMenu9.setText("Dueños");
+
+        jCheckBoxMenuItem10.setText("Nuevo");
+        jCheckBoxMenuItem10.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBoxMenuItem10ActionPerformed(evt);
+            }
+        });
+        jMenu9.add(jCheckBoxMenuItem10);
+
+        jCheckBoxMenuItem11.setText("Eliminar");
+        jMenu9.add(jCheckBoxMenuItem11);
+
+        jCheckBoxMenuItem12.setText("Modificar");
+        jCheckBoxMenuItem12.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBoxMenuItem12ActionPerformed(evt);
+            }
+        });
+        jMenu9.add(jCheckBoxMenuItem12);
+
+        jMenu6.add(jMenu9);
+
+        jMenu11.setText("Usuarios");
+
+        jCheckBoxMenuItem13.setText("Nuevo");
+        jCheckBoxMenuItem13.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBoxMenuItem13ActionPerformed(evt);
+            }
+        });
+        jMenu11.add(jCheckBoxMenuItem13);
+
+        jCheckBoxMenuItem14.setText("Eliminar");
+        jMenu11.add(jCheckBoxMenuItem14);
+
+        jCheckBoxMenuItem15.setText("Modificar");
+        jCheckBoxMenuItem15.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBoxMenuItem15ActionPerformed(evt);
+            }
+        });
+        jMenu11.add(jCheckBoxMenuItem15);
+
+        jMenu6.add(jMenu11);
+
+        jMenu12.setText("Calendario");
+
+        jCheckBoxMenuItem16.setText("Nuevo");
+        jMenu12.add(jCheckBoxMenuItem16);
+
+        jCheckBoxMenuItem17.setText("Eliminar");
+        jMenu12.add(jCheckBoxMenuItem17);
+
+        jCheckBoxMenuItem18.setText("Modificar");
+        jMenu12.add(jCheckBoxMenuItem18);
+
+        jMenu6.add(jMenu12);
+
+        jMenuBar1.add(jMenu6);
 
         setJMenuBar(jMenuBar1);
 
@@ -48,15 +223,43 @@ public class Principal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1)
+            .addGap(0, 303, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1)
+            .addGap(0, 111, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jCheckBoxMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxMenuItem4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBoxMenuItem4ActionPerformed
+
+    private void jCheckBoxMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxMenuItem5ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBoxMenuItem5ActionPerformed
+
+    private void jCheckBoxMenuItem10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxMenuItem10ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBoxMenuItem10ActionPerformed
+
+    private void jCheckBoxMenuItem12ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxMenuItem12ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBoxMenuItem12ActionPerformed
+
+    private void jCheckBoxMenuItem13ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxMenuItem13ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBoxMenuItem13ActionPerformed
+
+    private void jCheckBoxMenuItem15ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxMenuItem15ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBoxMenuItem15ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -94,9 +297,42 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem1;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem10;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem11;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem12;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem13;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem14;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem15;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem16;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem17;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem18;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem2;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem3;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem4;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem5;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem6;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem7;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem8;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem9;
     private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu10;
+    private javax.swing.JMenu jMenu11;
+    private javax.swing.JMenu jMenu12;
     private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenu jMenu7;
+    private javax.swing.JMenu jMenu8;
+    private javax.swing.JMenu jMenu9;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
     // End of variables declaration//GEN-END:variables
 }
